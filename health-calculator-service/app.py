4a45f6f8e27bd3fe212ac41285823d698db17568
cdetelder <camille.detelder03@gmail.com>
2024-11-30 18:27:22 +0000

    calculating errors fixed + form changed

--- health-calculator-service/app.py
+++ health-calculator-service/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask import Flask, request, jsonify, render_template
 from health_utils import calculate_bmi, calculate_bmr
 
 app = Flask(__name__)
@@ -7,21 +7,22 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/bmi-page')
-def bmi_page():
+@app.route('/bmi-form', methods=['GET'])
+def bmi_form():
     return render_template('bmi.html')
 
-@app.route('/bmr-page')
-def bmr_page():
+@app.route('/bmr-form', methods=['GET'])
+def bmr_form():
     return render_template('bmr.html')
 
 @app.route('/bmi', methods=['POST'])
 def bmi():
     height = float(request.form['height'])
     weight = float(request.form['weight'])
-    result = calculate_bmi(height, weight)
-    return render_template('bmi.html', bmi=result)
 
+    result = calculate_bmi(height, weight)
+
+    return render_template('bmi.html', bmi=result)
 
 @app.route('/bmr', methods=['POST'])
 def bmr():
@@ -29,7 +30,9 @@ def bmr():
     weight = float(request.form['weight'])
     age = int(request.form['age'])
     gender = request.form['gender']
+
     result = calculate_bmr(height, weight, age, gender)
+
     return render_template('bmr.html', bmr=result)
 
 if __name__ == '__main__':
